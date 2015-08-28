55ae4dbeb1c49ed74d0adfe07722deba1ff2b948
Pietro Berkes <pberkes@enthought.com>
2015-08-28 23:31:34 +0200

    Hands'on exercise for profiling ("factorial")

---
+++ hands_on/factorial/factorial.py
@@ -0,0 +1,33 @@
+""" Compute the factorial of a set of numbers stored in a file. """
+
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return factorial(n-1) * n
+
+
+def read_data(filename):
+    numbers = []
+    with open(filename, 'r') as f:
+        for line in f:
+            number = int(line)
+            numbers.append(number)
+    return numbers
+
+
+def compute_factorials_for_list(numbers):
+    factorials = []
+    for number in numbers:
+        result = factorial(number)
+        factorials.append(result)
+    return factorials
+    
+
+def main():
+    numbers = read_data('numbers.txt')
+    factorials = compute_factorials_for_list(numbers)
+
+
+if __name__ == '__main__':
+    main()
---
+++ hands_on/factorial/test_factorial.py
@@ -0,0 +1,18 @@
+""" Tests for the factorial function. """
+
+import unittest
+from factorial import factorial
+
+
+class FactorialTestCase(unittest.TestCase):
+    
+    def test_factorial(self):
+        factorial_cases = [(1, 1),
+                           (5, 2*3*4*5),
+                           (30, 265252859812191058636308480000000)]
+        for n, fact_n in factorial_cases:
+            self.assertEqual(factorial(n), fact_n)
+
+
+if __name__ == '__main__':
+    unittest.main()
