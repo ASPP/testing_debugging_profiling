0ff89b59805b13af18b06ab06f0adb0e2856c087
Pietro Berkes <pberkes@enthought.com>
2015-08-28 23:42:03 +0200

    Debugger demo

---
+++ hands_on/debugger/debugger_example.py
@@ -0,0 +1,20 @@
+
+def add(arg1, arg2):
+    return arg1 + arg2
+
+def sub(arg1, arg2):
+    return arg1 - arg2
+
+def div(arg1, arg2):
+    return arg1 / arg2
+
+def sum_over_difference(arg1, arg2):
+    """Compute sum of arguments over difference of arguments."""
+    arg_sum = add(arg1, arg2)
+    arg_diff = sub(arg1, arg2)
+    sum_over_diff = div(arg_sum, arg_diff)
+    return sum_over_diff
+
+if __name__ == '__main__':
+    result = sum_over_difference(12., 12.)
+    print result
