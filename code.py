80774c4bace9455d662b6d183c4155df2a9179fe
Pietro Berkes <pberkes@enthought.com>
2016-09-01 20:58:02 +0100

    Convert `hands_on` to py.test

--- code.py
+++ code.py
@@ -104,22 +104,3 @@ def test_lower1():
         output = string.lower()
         # Then
         assert output == expected
-
-
-def test_var_deterministic():
-    x = numpy.array([-2.0, 2.0])
-    expected = 4.0
-    assert isclose(numpy.var(x), expected)
-
-
-def test_var_fuzzing():
-    rand_state = numpy.random.RandomState(8393)
-
-    N, D = 100000, 5
-    # Goal variances: [0.1 ,  0.45,  0.8 ,  1.15,  1.5]
-    expected = numpy.linspace(0.1, 1.5, D)
-
-    # Generate random, D-dimensional data
-    x = rand_state.randn(N, D) * numpy.sqrt(expected)
-    variance = numpy.var(x, axis=0)
-    numpy.testing.assert_allclose(variance, expected, rtol=1e-2)
--- hands_on/factorial/test_factorial.py
+++ hands_on/factorial/test_factorial.py
@@ -1,19 +1,13 @@
 """ Tests for the factorial function. """
 
-import unittest
 from factorial import factorial
 
 
-class FactorialTestCase(unittest.TestCase):
-    
-    def test_factorial(self):
-        factorial_cases = [(1, 1),
-                           (0, 1),
-                           (5, 2*3*4*5),
-                           (30, 265252859812191058636308480000000)]
-        for n, fact_n in factorial_cases:
-            self.assertEqual(factorial(n), fact_n)
+def test_factorial():
+    factorial_cases = [(1, 1),
+                       (0, 1),
+                       (5, 2*3*4*5),
+                       (30, 265252859812191058636308480000000)]
 
-
-if __name__ == '__main__':
-    unittest.main()
+    for n, fact_n in factorial_cases:
+        assert factorial(n) == fact_n
