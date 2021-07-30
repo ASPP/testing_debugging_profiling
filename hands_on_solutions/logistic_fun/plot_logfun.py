7112418fa9f04010532e1260c8f01fb28bced36c
Lisa Schwetlick <lisa.schwetlick@uni-potsdam.de>
2021-07-30 21:43:06 +0200

    wrote all the solutions and exercises

--- hands_on_solutions/logistic_fun/plot_logfun.py
+++ hands_on_solutions/logistic_fun/plot_logfun.py
@@ -3,31 +3,63 @@ from logistic import iterate_f
 from matplotlib import pyplot as plt
 
 
-n_conv = 15
-l1 = iterate_f(n_conv, 0.1, 1.5)
-l2 = iterate_f(n_conv, 0.3, 1.5)
-l3 = iterate_f(n_conv, 0.5, 1.5)
+def plot_trajectory(n, r, x0, fname="single_trajectory.png"):
+    l = iterate_f(n, x0, r)
+    fig, ax = plt.subplots(figsize=(10,5))
+    ax.plot(list(range(n)), l)
+    fig.suptitle('Logistic Function')
+
+    fig.savefig(fname)
+    return fig, ax
+
+def plot_bifurcation(start, end, step, fname="bifurcation.png"):
+    r_range=np.arange(start, end, step)
+    it = 100000
+    last = 200
+    x=[]
+    y=[]
+    #rrannge
+    for r in r_range:
+        l = iterate_f(it, 0.1, r)
+        ll = l[len(l)-last::].copy()
+        lll = np.unique(ll)
+        y.extend(lll)
+        x.extend(np.ones(len(lll))*r)
+
+    fig, ax = plt.subplots(figsize=(20,10))
+    ax.scatter(x,y, s=0.1)
+    ax.set_xlabel("r")
+    fig.savefig(fname)
+    return fig, ax
 
 
-n_div = 15
-l4 = iterate_f(n_div, 0.1, 4)
-l5 = iterate_f(n_div, 0.11, 4)
-l6 = iterate_f(n_div, 0.111, 4)
+if __name__=="__main__":
+    n_conv = 15
+    l1 = iterate_f(n_conv, 0.1, 1.5)
+    l2 = iterate_f(n_conv, 0.3, 1.5)
+    l3 = iterate_f(n_conv, 0.5, 1.5)
 
+    n_div = 15
+    l4 = iterate_f(n_div, 0.1, 4)
+    l5 = iterate_f(n_div, 0.11, 4)
+    l6 = iterate_f(n_div, 0.111, 4)
 
-fig, ax = plt.subplots(1,2, figsize=(10,5))
-ax[0].plot(list(range(n_conv)), l1, label=str(0.1))
-ax[0].plot(list(range(n_conv)), l2, label=str(0.3))
-ax[0].plot(list(range(n_conv)), l3, label=str(0.5))
-ax[0].legend()
-ax[0].set_title("r = 1.5")
+    fig, ax = plt.subplots(1,2, figsize=(10,5))
+    ax[0].plot(list(range(n_conv)), l1, label=str(0.1))
+    ax[0].plot(list(range(n_conv)), l2, label=str(0.3))
+    ax[0].plot(list(range(n_conv)), l3, label=str(0.5))
+    ax[0].legend()
+    ax[0].set_title("r = 1.5")
 
-ax[1].plot(list(range(n_div)), l4, label=str(0.1))
-ax[1].plot(list(range(n_div)), l5, label=str(0.11))
-ax[1].plot(list(range(n_div)), l6, label=str(0.111))
-ax[1].legend()
-ax[1].set_title("r = 4")
+    ax[1].plot(list(range(n_div)), l4, label=str(0.1))
+    ax[1].plot(list(range(n_div)), l5, label=str(0.11))
+    ax[1].plot(list(range(n_div)), l6, label=str(0.111))
+    ax[1].legend()
+    ax[1].set_title("r = 4")
 
-fig.suptitle('Logistic Function')
+    fig.suptitle('Logistic Function')
 
-fig.savefig("log_fun.png")
+    fig.savefig("log_fun.png")
+    plot_bifurcation(1, 4, 0.01, fname="bifurcation.png")
+    plot_bifurcation(3, 4, 0.003, fname="bifurcation_zoom.png")
+    plot_trajectory(300, 3.5, 0.1, fname="single_trajectory.png")
--- hands_on_solutions/logistic_fun/test_logistic.py
+++ hands_on_solutions/logistic_fun/test_logistic.py
@@ -1,40 +1,102 @@
 import numpy as np
-from logistic import iterate_f
+from logistic import f, iterate_f
 import pytest
 
-@pytest.mark.parametrize("x0", [0.1, 0.5,0.9])
-def test_convergence(x0):
-    expected_value = 1/3
-    l = iterate_f(1000, x0, 1.5)
-    conv_value = np.mean(l[-10:-1])
-
-    assert np.isclose(expected_value, conv_value)
-
-
+# Exercise 1: write a basic test, that checks that once a point is on the attractor
+# it doesnt move off it! For an r = 1.5 the attracting fixed point is at 1/3!
 def test_stability():
+    """
+    Checks that if we iterate on an attractor, the output is the same as the
+    input
+    """
     x0 = 1/3
     expected_value = 1/3
+    r = 1.5
+    result = f(x0, r)
+    assert np.isclose(expected_value, result)
+
+# Exercise 2: Now write a test for checking that with different starting values
+# the trajectories all converge on the same attractor. Use the same r value as
+# above but use parametrize to try a few different starting values.
+@pytest.mark.parametrize("x0", [0.1, 0.5,0.9])
+def test_convergence(x0):
+    """
+    Checks that for a specific r < 3, given different initial values x0, all
+    converge to the same attractor after 1000 iterations.
+    """
+    expected_value = 1/3
+    r = 1.5
     l = iterate_f(1000, x0, 1.5)
     conv_value = np.mean(l[-10:-1])
     assert np.isclose(expected_value, conv_value)
 
-def test_divergence():
+# Exercise 3: trajectories using r values above 4 diverge to negative infinity.
+# pick an r value above 5 and check that a few iterations later the outcome is
+# -inf for different starting values.
+# Exercise 3.2: expand your test to use numerical fuzzing to pick an arbitrary
+# value of r > 4.
+
+@pytest.mark.parametrize("x0", [0.1, 0.5,0.9])
+def test_divergence(x0):
+    """
+    Checks that for values of r > 4, given different initial values x0, all
+    diverge to infinity after 1000 iterations.
+    """
     rand_state = np.random.RandomState(8393)
-    r = rand_state.uniform(4,6)
-    x0 = rand_state.rand()
-    n = 10
-    l_x = iterate_f(n, x0, r)
-    l_x_diff = np.diff(l_x)
-    err = 0.000001
-    assert not all(l_x_diff[-10:-1]>err)
+    r = rand_state.uniform(low=4, high=15)
+    expected_value = -np.inf
+    #r = 5
+    l = iterate_f(1000, x0, r)
+    assert np.isclose(expected_value, l[-1])
+
+# Exercise 4: r values or 3 < r > 4 have some interesting properties. A chaotic
+# trajectory doesn't diverge but also doesn't converge. Write a test that checks
+# that after a lot (e.g. 100000) of iterations the last 100 are all different.
+# use r=3.8.
+# Exercise 4.2: parametrize your test with some other r values: like 3.001,
+# and 3.453. Your test should fail. Why? Use the plotting function
+# `plot_trajectory` to find out what is going on. Then mark your test as
+# expected to fail.
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("r", [3.8, 3.001, 3.453])
+def test_aperiodic(r):
+    """
+    Checks that for a specific 3 < r > 4 trajectories become chaotic
+    """
+    #r = 3.8
+    l = iterate_f(100000, 0.01, r)
+    last = l[-101:-1]
+    assert len(np.unique(last))==100
+
+# Exercise 5: To test chaotic behavior we will need to be a bit more advanced.
+# Let's test that what we're seeing is actually chaos:
+# - orbits mus be bounded, i.e. not diverge: you can use your divergence code for this
+# - orbits must be aperiodic, i.e. only values of r that pass the test_aperiodic function can qualify
+# - sensitive dependence on initial conditions
+# - it has to be deterministic
+# Lets write tests for the last two!
+# Exercise 5.1: Look at the bifurcation plot and single trajectory plot
+# and pick an r value that you think will likely yield chaos. Then write a test
+# to verify that the trajectory is deterministic.
+
+def test_determinism():
+    x0 = 1/3
+    r = 3.8
+    result1 = f(x0, r)
+    result2 = f(x0, r)
+    assert np.allclose(result1, result2)
+
+# Exercise 5.2: for the same r value, test the sensitive dependence on initial
+# conditions, or the butterfly effect. Use the following definition of SDIC.
 
 @pytest.mark.parametrize("i", range(10))
 def test_fuzzy_sdic(i):
     """
-    there is a number delta such that for any x0 there is a y0 that is not more 
+    there is a number delta such that for any x0 there is a y0 that is not more
     than init_error away from x0, when the initial condition y0 has the property
-    that there is an int n wuch that after n iterations the orbit is more than 
-    delta away from the orbit of x0. That is 
+    that there is an int n wuch that after n iterations the orbit is more than
+    delta away from the orbit of x0. That is
     |xn-yn| > delta
     """
     rand_state = np.random.RandomState(8393)
@@ -43,8 +105,8 @@ def test_fuzzy_sdic(i):
     init_error = rand_state.rand()
     y0max = x0 + init_error
     n = 10000
-    l_x = iterate_f(n, x0, 4)
-    l_y = iterate_f(n, y0max, 4)
+    l_x = iterate_f(n, x0, 3.8)
+    l_y = iterate_f(n, y0max, 3.8)
     assert any(abs(l_x - l_y)>delta)
 
 
