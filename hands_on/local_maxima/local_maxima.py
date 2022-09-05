dc027f80e3e5b6183c71dd718c4f6a8f87cc47a0
Pietro Berkes <pietro.berkes@googlemail.com>
2022-09-06 00:23:18 +0200

    Add exercise to finish the `local_maxima`

---
+++ hands_on/local_maxima/local_maxima.py
@@ -0,0 +1,22 @@
+def local_maxima(x):
+    """Find local maxima of x.
+
+    Example:
+    >>> x = [1, 3, -2, 0, 2, 1]
+    >>> find_maxima(x)
+    [1, 4]
+
+    If in a local maximum several elements have the same value,
+    return the left-most index.
+    Example:
+    >>> x = [1, 2, 2, 1]
+    >>> find_maxima(x)
+    [1]
+
+    Input arguments:
+    x -- 1D list of real numbers
+
+    Output:
+    idx -- list of indices of the local maxima in x
+    """
+    return []
---
+++ hands_on/local_maxima/test_local_maxima.py
@@ -0,0 +1,27 @@
+from local_maxima import local_maxima
+
+
+def test_local_maxima():
+    values = [1, 3, -2, 0, 2, 1]
+    expected = [1, 4]
+    maxima = local_maxima(values)
+    assert maxima == expected
+
+
+def test_local_maxima_negative():
+    values = [-1, -1, 0, -1]
+    expected = [2]
+    maxima = local_maxima(values)
+    assert maxima == expected
+
+
+def test_local_maxima_edges():
+    values = [4, 2, 1, 3, 1, 5]
+    expected = [0, 3, 5]
+    maxima = local_maxima(values)
+    assert maxima == expected
+
+
+def test_local_maxima_plateau():
+    raise Exception('not yet implemented')
+ 
