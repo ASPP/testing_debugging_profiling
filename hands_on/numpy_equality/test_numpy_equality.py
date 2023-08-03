b999732ab1f253a8d0f2ac6c99581487474d900b
Pietro Berkes <pietro.berkes@googlemail.com>
2023-08-03 11:04:14 +0200

    numpy equality, including examples with nans

--- hands_on/numpy_equality/test_numpy_equality.py
+++ hands_on/numpy_equality/test_numpy_equality.py
@@ -1,23 +1,22 @@
 import numpy as np
-from numpy.testing import assert_allclose, assert_equal
 
 
 def test_equality():
     x = np.array([1, 1])
     y = np.array([2, 2])
     z = np.array([3, 3])
-    assert_equal(x + y, z)
+    assert x + y == z
 
 
 def test_equality_with_nan():
     x = np.array([1, np.nan])
     y = np.array([2, np.nan])
     z = np.array([3, np.nan])
-    assert_equal(x + y, z)
+    assert x + y == z
 
 
 def test_allclose_with_nan():
     x = np.array([1.1, np.nan])
     y = np.array([2.2, np.nan])
     z = np.array([3.3, np.nan])
-    assert_allclose(x + y, z)
+    assert x + y == z
