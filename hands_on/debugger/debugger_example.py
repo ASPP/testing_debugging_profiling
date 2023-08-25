e0f638a93ac360fb75e3be83c063aef6685bc92b
Pietro Berkes <pietro.berkes@googlemail.com>
2023-08-25 11:03:57 +0200

    Debugging example, add jupyter notebook

--- hands_on/debugger/debugger_example.py
+++ hands_on/debugger/debugger_example.py
@@ -1,12 +1,14 @@
-
 def add(arg1, arg2):
     return arg1 + arg2
+
 
 def sub(arg1, arg2):
     return arg1 - arg2
 
+
 def div(arg1, arg2):
     return arg1 / arg2
+
 
 def sum_over_difference(arg1, arg2):
     """Compute sum of arguments over difference of arguments."""
@@ -17,5 +19,5 @@ def sum_over_difference(arg1, arg2):
 
 
 if __name__ == '__main__':
-    result = sum_over_difference(12., 12.)
+    result = sum_over_difference(7, 5)
     print(result)
