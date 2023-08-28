84debcf0cd9fe700210333674fcd8f393e5061cf
Pietro Berkes <pietro.berkes@googlemail.com>
2023-08-28 09:41:44 +0300

    Create part2 directory for local_maxima exercise

---
+++ hands_on/local_maxima_part2/local_maxima.py
@@ -0,0 +1,10 @@
+def find_maxima(x):
+    """Find local maxima of x.
+
+    Input arguments:
+    x -- 1D list of real numbers
+
+    Output:
+    idx -- list of indices of the local maxima in x
+    """
+    return []
