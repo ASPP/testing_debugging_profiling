d91a268ac4126f367b0951f67b69d1a1649f5ffb
Pietro Berkes <pietro.berkes@googlemail.com>
2026-07-26 16:14:44 -0600

    Local maxima debug files for teacher

---
+++ hands_on/local_maxima_part3_debug_teacher/local_maxima.py
@@ -0,0 +1,51 @@
+def find_maxima(x):
+    """Find local maxima of x.
+
+    Input arguments:
+    x -- 1D list of real numbers
+
+    Output:
+    idx -- list of indices of the local maxima in x
+    """
+
+    maxima = []
+
+    len_x = len(x)
+    if len_x == 0:
+        return maxima
+
+    maxima = check_first_element(x, maxima)
+    maxima = check_last_element(x, maxima)
+
+    # Check numbers in between
+    i = 1
+    while i < len_x - 1:
+        if x[i] > x[i - 1]:
+            # We have found a potential maximum or start of a plateau
+            plateau_start = i
+            while i < len_x - 1 and x[i] == x[i + 1]:
+                i += 1
+            plateau_end = i
+            if x[plateau_end] > x[plateau_end + 1]:
+                maxima.append(plateau_start)
+        i += 1
+    return maxima
+
+
+def check_first_element(x, maxima):
+    if x[0] > x[1]:
+        maxima.append(0)
+    return maxima
+
+
+def check_last_element(x, maxima):
+    if x[-1] > x[-2]:
+        maxima.append(len(x) - 1)
+    return maxima
+
+
+if __name__ == "__main__":
+    numbers = [1, 2, 2, 1]
+    print('Input:', numbers)
+    result = find_maxima(numbers)
+    print('Indices of local maxima:', result)
---
+++ hands_on/local_maxima_part3_debug_teacher/test_local_maxima.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pytest
+
+from local_maxima import find_maxima
+
+
+def test_find_maxima():
+    values = [1, 3, -2, 0, 2, 1]
+    expected = [1, 4]
+    maxima = find_maxima(values)
+    assert maxima == expected
+
+
+def test_find_maxima_edges():
+    values = [4, 2, 1, 0, 1, 5]
+    expected = [0, 5]
+    maxima = find_maxima(values)
+    assert maxima == expected
+
+
+def test_find_maxima_empty():
+    values = []
+    expected = []
+    maxima = find_maxima(values)
+    assert maxima == expected
+
+
+def test_find_maxima_plateau():
+    values = [1, 2, 2, 1]
+    expected = [1]
+    maxima = find_maxima(values)
+    assert maxima == expected
+
+
+def test_find_maxima_not_a_plateau():
+    values = [1, 2, 2, 3, 1]
+    expected = [3]
+    maxima = find_maxima(values)
+    assert maxima == expected
